eb9b10ddff65510394dfb3e1ca393ae1588cf95e
sherbakov <ashcherbakov@beeline.ru>
2014-10-29 17:58:00 +0300

    add recursive

--- rnd_pass.py
+++ rnd_pass.py
@@ -6,5 +6,9 @@ def id_generator(size):
 	chars = string.letters+string.digits
 	return ''.join(random.choice(chars) for _ in range(size))
 
-size = int(raw_input("enter word size: "))
-print id_generator(size)
+size = 8
+
+while 1:
+	#size = int(raw_input("enter pass size: "))
+	print id_generator(size)
+	raw_input("Press Enter")
